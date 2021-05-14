f24a679bb1c4165033334e78028d7e1e467ed4e6
hdkhh <hyunny1@gmail.com>
2021-05-15 01:48:24 +0900

    mixins

---
+++ src/mixins/numberFormat.js
@@ -0,0 +1,16 @@
+export const numberFormat = {
+    data () {
+        return {
+
+        }
+    },
+    methods: {
+        numberWithCommas: function (x) {
+            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+        },
+        numberDelCommas: function (str) {
+            str = String(str)
+            return str.replace(/[^\d]+/g, '')
+        } 
+    }
+}
